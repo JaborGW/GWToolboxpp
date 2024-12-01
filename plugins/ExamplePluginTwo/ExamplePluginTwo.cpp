63c3aa89f31865c9e79661663ac7a6b12f2a6fd8
Jabor <lja@posteo.de>
2024-12-01 10:39:17 +0100

    Add second example plugin

---
+++ plugins/ExamplePluginTwo/ExamplePluginTwo.cpp
@@ -0,0 +1,74 @@
+#include "ExamplePluginTwo.h"
+
+#include <GWCA/Constants/Constants.h>
+#include <GWCA/Managers/MapMgr.h>
+
+#include <Utils/GuiUtils.h>
+#include <GWCA/GWCA.h>
+#include <GWCA/Utilities/Hooker.h>
+#include "GWCA/Managers/ChatMgr.h"
+
+namespace {
+    bool redirect_slash_ee_to_eee = false;
+}
+
+DLLAPI ToolboxPlugin* ToolboxPluginInstance()
+{
+    static ExamplePluginTwo instance;
+    return &instance;
+}
+
+void ExamplePluginTwo::LoadSettings(const wchar_t* folder)
+{
+    ToolboxUIPlugin::LoadSettings(folder);
+    PLUGIN_LOAD_BOOL(redirect_slash_ee_to_eee);
+}
+
+void ExamplePluginTwo::SaveSettings(const wchar_t* folder)
+{
+    ToolboxUIPlugin::SaveSettings(folder);
+    PLUGIN_SAVE_BOOL(redirect_slash_ee_to_eee);
+    PLUGIN_ASSERT(ini.SaveFile(GetSettingFile(folder).c_str()) == SI_OK);
+}
+
+void ExamplePluginTwo::DrawSettings()
+{
+    if (!toolbox_handle) {
+        return;
+    }
+    ImGui::Checkbox("Redirect ee to eee", &redirect_slash_ee_to_eee);
+}
+
+void ExamplePluginTwo::Initialize(ImGuiContext* ctx, const ImGuiAllocFns allocator_fns, const HMODULE toolbox_dll)
+{
+    ToolboxUIPlugin::Initialize(ctx, allocator_fns, toolbox_dll);
+    GW::Chat::CreateCommand(L"ee", [](GW::HookStatus*, const wchar_t*, const int, const LPWSTR*) {
+        if (redirect_slash_ee_to_eee) {
+            GW::Chat::SendChat('/', "eee");
+        }
+    });
+}
+void ExamplePluginTwo::SignalTerminate()
+{
+    ToolboxUIPlugin::SignalTerminate();
+    GW::Chat::DeleteCommand(L"ee");
+    GW::DisableHooks();
+}
+
+bool ExamplePluginTwo::CanTerminate() {
+    return GW::Hook::GetInHookCount() == 0;
+}
+
+void ExamplePluginTwo::Draw(IDirect3DDevice9*)
+{
+    if (GW::Map::GetInstanceType() != GW::Constants::InstanceType::Loading) {
+        return;
+    }
+
+    ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0, 0, 0, 0));
+    if (ImGui::Begin(Name(), GetVisiblePtr(), ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoScrollbar)) {
+        ImGui::Text("Example plugin: area loading...");
+    }
+    ImGui::End();
+    ImGui::PopStyleColor();
+}
